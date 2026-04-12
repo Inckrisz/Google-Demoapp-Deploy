f1291c85a1dfa8d02152c554851a338b594c695c
Inckrisz <inckrisz28@gmail.com>
2026-04-12 02:14:13 +0200

    changed html, added workflows

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -819,7 +819,7 @@ module "ecs_service_frontend" {
   cluster_arn = module.ecs_cluster.cluster_arn
   subnet_ids = module.network.public_subnet_ids
   name_prefix = var.name_prefix
-  target_group_arn = aws_lb_listener.http.arn
+  target_group_arn = aws_lb_target_group.frontend.arn
   # iam_role = aws_iam_role.ecs_task_execution_role.arn
 
   depends_on = [ aws_lb_listener.http ]
